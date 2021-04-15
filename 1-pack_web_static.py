3553b84c2065ebc1061cc3cdce5385808f8135a9
angel19951 <2384@holbertonschool.com>
2021-04-15 18:47:11 +0000

    Fixed error on file name

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,7 +12,7 @@ def do_pack():
     Generates a .tgz archive from the contents in web_static
     """
     create_time = datetime.datetime.utcnow().strftime("%Y%m%d%H%M%S")
-    file_name = "web_static_{}.tgz".format(create_time)
+    file_name = "versions/web_static_{}.tgz".format(create_time)
     try:
         fabric.api.local("mkdir -p /versions")
         fabric.api.local("tar -cvzf {} web_static".format(file_name))
