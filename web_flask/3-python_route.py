d1838c12bb763932d7474f0d7fea997cc44ca0b7
Angel <2384@holbertonschool.com>
2021-05-02 23:49:45 +0000

    Adding extra route for python, and variable processing

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Script to start a Flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+host = '0.0.0.0'
+port = '5000'
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """
+    Return Hello HBNB!
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """
+    Return HBNB
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_c(text):
+    """
+    Return C is <>
+    """
+    rm_text_dash = text.replace('_', ' ')
+    return 'C {}'.format(rm_text_dash)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def hello_python(text='is cool'):
+    """
+    Return python is cool unless user changes entry
+    """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+if __name__ == '__main__':
+    app.run(host, port)
